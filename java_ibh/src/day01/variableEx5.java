024dae279ff22c85cba4403d7bd35208f5921bdd
imbyeonghun <byeonghun6885@naver.com>
2023-12-08 16:21:53 +0900

    variableEx_문자열

---
+++ java_ibh/src/day01/variableEx5.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class variableEx5 {
+
+	// 문자열 변수 선언 예제
+	public static void main(String[] args) {
+
+		// String 클래스 : 문자열을 저장하는 클래스로 다양한 기능을 제공
+		// 참조변수는 null 사용 가능
+		
+		
+		
+	}
+}
