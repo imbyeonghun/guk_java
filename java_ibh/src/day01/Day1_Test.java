156c86f73e679c30f94267059b22ef5586006caf
imbyeonghun <byeonghun6885@naver.com>
2023-12-08 17:36:27 +0900

    day1_test

---
+++ java_ibh/src/day01/Day1_Test.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Day1_Test {
+
+	public static void main(String[] args) {
+
+		// 1. char ch1 = "A"; 에서 틀린부분을 찾아 수정
+		char ch1 = 'A';
+		String ch2 = "" + 'A';
+		System.out.println(ch1);
+		System.out.println(ch2);
+		
+		// 2. 국어,영어,수학 성적 입력하여 평균을 구하려하는데
+		// 이때 필요한 변수들 선언
+		int korea, eng, math,total;
+	}
+}
