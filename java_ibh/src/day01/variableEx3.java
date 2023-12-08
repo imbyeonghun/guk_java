1c2eb290aab718d19f3bc7bc602e7bc95ca5f972
imbyeonghun <byeonghun6885@naver.com>
2023-12-08 16:21:37 +0900

    variableEx_실수형

---
+++ java_ibh/src/day01/variableEx3.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class variableEx3 {
+
+	// 실수형 변수 선언 예제
+	public static void main(String[] args) {
+
+		// float 4byte, 소수점 6자리 접미사, f & F사용
+		// double 8바이트, 소수점 15자리
+		
+		// 입력한 값들을 리터럴이라 하고
+		// 실수 리터럴은 기본타입이 double
+		float num1 = 1.23F;
+		System.out.println(num1);
+		
+		double num2 = 3.14;
+		System.out.println(num2);
+	}
+}
