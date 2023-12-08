5c5f3be1280e5db5dfe6f4dada4556daf19ae340
imbyeonghun <byeonghun6885@naver.com>
2023-12-08 16:21:45 +0900

    variableEx_논리형

---
+++ java_ibh/src/day01/variableEx4.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class variableEx4 {
+
+	// 논리형 변수 선언 예제
+	public static void main(String[] args) {
+
+		boolean isEven1 = true;
+		System.out.println(isEven1);
+		
+		boolean isEven2 = false;
+		System.out.println(isEven2);
+		
+		boolean isEven = 3 % 2 == 0;
+		System.out.println("3 % 2 == 0? " + isEven);
+	}
+
+}
