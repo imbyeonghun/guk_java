d72f42b9b680f99a4373fdb82b849d26ecf31a8c
imbyeonghun <byeonghun6885@naver.com>
2023-12-13 17:16:16 +0900

    무한 루프 예제1

---
+++ java_ibh/src/day04/InfiniteLoopEx1.java
@@ -0,0 +1,31 @@
+package day04;
+
+import java.util.Scanner;
+
+public class InfiniteLoopEx1 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		// 무한 루프 예제1 : 조건식을 참으로 만들어서 무한루프 발생
+		while(true) {
+			System.out.println("정수 입력 : ");
+			int num = scan.nextInt();
+			System.out.println("정수 출력 : " + num);
+		}
+		/*
+		무한루프가 100% 발생하는 코드 다음에 있는 
+		코드는 에러가 발생
+		이유 : 절대 실행될 일이 없음
+		System.out.println("프로그램 종료");
+		
+		무한 루프 예제2 : 조건식을 참으로 만들어서 무한루프 발생
+		for( ; ; ) {
+			System.out.println("정수 입력 : ");
+			int num = scan.nextInt();
+			System.out.println("정수 출력 : " + num);
+		}
+		*/
+	}
+}
