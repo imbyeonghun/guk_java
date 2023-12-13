5f7e4a8176894785f6126d9525480f983a769268
imbyeonghun <byeonghun6885@naver.com>
2023-12-13 09:27:03 +0900

    1부터 5까지 출력되는 코드

---
+++ java_ibh/src/day03/WhileTestEx1.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class WhileTestEx1 {
+
+	// 1부터 5까지 출력되는 코드를 작성하세요.
+	public static void main(String[] args) {
+
+		int num = 1;
+		int count = 5;
+		while(num <= count) {
+			System.out.println(num);
+			num++;
+		}
+	}
+}
