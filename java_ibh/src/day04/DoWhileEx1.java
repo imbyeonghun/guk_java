34873c647e2986ab1e0aa68aff3f935ffb5fefc2
imbyeonghun <byeonghun6885@naver.com>
2023-12-13 15:38:22 +0900

    q를 입력하기 전까지 문자를 입력하면 입력받은 문자를 출력하는 예제

---
+++ java_ibh/src/day04/DoWhileEx1.java
@@ -0,0 +1,22 @@
+package day04;
+
+import java.util.Scanner;
+
+public class DoWhileEx1 {
+
+	public static void main(String[] args) {
+		// q를 입력하기 전까지 문자를 입력하면 입력받은 문자를 출력하는 예제
+		
+		Scanner scan = new Scanner(System.in);
+		
+		char ch;
+		
+		do {
+			System.out.print("문자를 입력하세요 : ");
+			ch = scan.next().charAt(0);
+			System.out.println("문자 : " + ch);
+		}while(ch != 'q');
+		
+		scan.close();
+	}
+}
