d6b1d26cffb6b5c25b7515b6acaa853ee8da5671
imbyeonghun <byeonghun6885@naver.com>
2023-12-15 17:50:14 +0900

    숫자 야구게임 과제

---
+++ java_ibh/src/day06/HomeWorkEx2.java
@@ -0,0 +1,173 @@
+package day06;
+
+import java.util.Scanner;
+
+public class HomeWorkEx2 {
+
+	public static void main(String[] args) {
+		/* 
+		 숫자 야구게임을 구현하세요.
+		  - 기록은 최대 5개. 이전 기록은 삭제
+		  - 1~9사이의 중복되지 않은 3개의 수를 랜덤으로 선택해서 해당 숫자를 맞추는 게임
+		  - S : 숫자가 있고, 위치가 같은 경우
+		  - B : 숫자가 있지만 위치가 다른 경우
+		  - O : 일치하는 수자가 하나도 없는 경우
+		  - 3S가 되면 게임이 종료
+		  
+		  메뉴
+		  1. 새게임
+		  2. 기록확인
+		  3. 프로그램 종료
+		  메뉴 선택 : 1
+		  
+		  (랜덤으로 생성된 숫자 : 3 9 1)
+		  입력 : 1 2 3
+		  결과 : 2B
+		  입력 : 4 5 6
+		  결과 : O
+		  입력 : 7 9 8
+		  결과 : 1S
+		  입력 : 3 1 9
+		  결과 : 1S2B
+		  입력 : 3 9 1
+		  결과 : 3S
+		  정답입니다.
+		  도전 횟수 : 5
+		  현재 1등입니다. 이름을 기록하세요.
+		  이름 : JIK
+		 
+		  메뉴
+		  1. 새게임
+		  2. 기록확인
+		  3. 프로그램 종료
+		  메뉴 선택 : 2
+		  --기록--
+		  1. JIK : 5회
+		  */
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int menu;
+
+		// 맞춰야하는 숫자 3개 랜덤으로 생성
+		int min = 1, max = 9;
+		int ran1 = (int)(Math.random() * (max - min + 1) - min);
+		int ran2 = (int)(Math.random() * (max - min + 1) - min);
+		int ran3 = (int)(Math.random() * (max - min + 1) - min);
+		int record = 0; // 기록
+		int S = 0, B = 0, O = 0; // s,b,o 초기화
+		
+		String[] bestRecord = new String[5]; // 상위 기록 5개 배열
+		String[] name = new String[5]; 		 // 이름 저장할 배열
+		
+		// 랜덤 3수를 배열에 넣고 입력받은 값도 배열에 넣고
+		int[] pass = new int[] {ran1,ran2,ran3};
+		int[] user = new int[3];
+		
+		int count = 0;
+		while(count < pass.length) {
+			// 랜덤 수 생성
+			int r = (int)(Math.random() * (max - min + 1) + min);
+			
+			// 중복하지 않게
+			int i;
+			for(i = 0; i<count; i++) {
+				if(r == pass[i]) {
+					break;
+				}
+			}
+			if(i == count) {
+				pass[count++] = r;
+			}
+		}
+				
+		System.out.print(pass[0]);
+		System.out.print(pass[1]);
+		System.out.println(pass[2]);
+		
+		//반복문 선택한 메뉴가 프로그램 종료가 아닐때까지
+		do {
+			//메인 메뉴출력
+			System.out.println("메뉴");
+			System.out.println("1.새 게임");
+			System.out.println("2.기록 확인");
+			System.out.println("3.프로그램 종료");
+			System.out.print("메뉴 선택 : ");
+			
+			//메인메뉴를 선택
+			menu = scan.nextInt();
+			
+			//선택한 메인메뉴에 따른 기능 실행
+			switch(menu) {
+			case 1:
+				do{
+					// 숫자 3개 입력받기
+					System.out.print("숫자 3개를 입력하세요 : ");
+					
+					int num1 = scan.nextInt();
+					int num2 = scan.nextInt();
+					int num3 = scan.nextInt();
+					
+					user = new int[] {num1,num2,num3};
+					
+					// 입력받은 값을 초기화 해준다.
+					S = 0;
+					B = 0;
+					O = 0;
+					
+					// 인자가 맞는지, 숫자가 맞는지 확인하고
+					for(int i = 0; i < pass.length; i++) {
+						if(pass[i] == user[i]) {
+							System.out.println("S");
+							S++;
+							continue;
+						}else if((pass[i] != user[i]) && ((user[i]) == pass[0] )) {
+							System.out.println("B");
+							B++;
+							continue;
+						}else if((pass[i] != user[i]) && ((user[i]) == pass[1] )) {
+							System.out.println("B");
+							B++;
+							continue;
+						}else if((pass[i] != user[i]) && ((user[i]) == pass[2] )) {
+							System.out.println("B");
+							B++;
+							continue;
+						}else {
+							System.out.println("O");
+							O++;
+							continue;
+						}
+					} // for
+					
+					System.out.println(S + "S " + B + "B " +  O + "O");
+					record++;
+					
+				}while(S != 3); // do
+				System.out.println("도전 횟수 : " + record);
+				System.out.println("이름을 기록하세요.");
+				System.out.print("이름 : ");
+				// nextLine() 주의 
+				// 이전 scan에서 next,nextint,nextLine에서 enter를 저장하고 있기 때문에 
+				// next,nextint는 엔터를 버리지만 nextLine은 엔터를 가져오기 때문에 조심
+				String naming = scan.next(); // 기록할 이름
+				break;
+				
+			case 2:
+				System.out.println("--기록--");
+				System.out.println(record);
+				// 상위 5개 기록 저장 후 출력
+				break;
+				
+			case 3:
+				System.out.println("프로그램을 종료합니다.");
+				break;
+				
+			default:
+				System.out.println("잘못된 메뉴입니다.");
+			}
+		}while( menu != 3);
+		
+		scan.close();
+	} // main
+} // class
