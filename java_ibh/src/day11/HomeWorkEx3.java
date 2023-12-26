26d7590b4283979f04a88599d04540d172e12211
imbyeonghun <byeonghun6885@naver.com>
2023-12-26 11:03:56 +0900

    사이트에서 회원 관리(로그인,회원가입 등)를 위한 Member클래스

--- java_ibh/src/day11/HomeWorkEx3.java
+++ java_ibh/src/day11/HomeWorkEx3.java
@@ -1,65 +1,31 @@
 package day11;
 
-import java.util.Scanner;
-
 public class HomeWorkEx3 {
 
 	//사이트에서 회원 관리(로그인,회원가입 등)를 위한 Member클래스를 작성하세요
-	private static Member list[] = new Member[5];	// 회원 배열
-	private static int count = 0; 					// 등록한 회원 숫자  
-	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		// 작성한 Member 클래스를 이용하여 인스턴스를 생성하고 구현한 매서드를 테스트해보세요
 		
-		System.out.print("아이디를 입력하세요 : ");
-		String id = scan.next();
-			
-		System.out.print("비밀번호를 입력하세요 : ");
-		String pass = scan.next();
+		Member mem1 = new Member();
+		mem1.print();
 		
-		Member user1 = new Member(id,pass);
-		list[count] = user1;
-		count++;
+		// 아이디와 비번이 없는 인스턴스이기 때문에 아이디 abc와 비번 def로 비교하면 false
+		System.out.println(mem1.eqauals("abc", "def"));
 		
-		System.out.println("로그인하시겠습니까?");
-		int menu = scan.nextInt();
-		
-		switch(menu) {
-		case 1:
-			System.out.print("아이디 : ");
-			String userid = scan.next(); 
-			
-			int index = -1;
-			for(int i = 0; i < count; i++) {
-				if(list[i].equals(userid)) {
-					index = i;
-				}
-			}
-			
-			System.out.print("비밀번호 : ");
-			String userpass = scan.next();
-			if(list[index].getPass() == userpass) {
-				System.out.print("로그인 완료!");
-			}else {
-				System.out.print("비밀번호를 다시 확인해주세요.");
-			}
-			break;
-		case 2:
-			break;
-		}
-		
-		scan.close();
+		// 아이디가 abc, 비번이 def, 이메일이 abc@naver.com인 인스턴스 생성
+		Member mem2 = new Member("abc", "def", "abc@naver.com");
+		mem2.print();
+		System.out.println(mem2.eqauals("abc", "def"));
 	}
 }
 
 class Member{
+	
 	// 필요한 멤버변수 및 메서드를 정리하고 구현하세요
 	// 변수
-	// 아이디,비밀번호
-	String id,pass;
+	// 아이디,비밀번호,이메일
+	String id,pass,eamil;
 	
-	// 메서드
+	// getter setter
 	public String getId() {
 		return id;
 	}
@@ -76,17 +42,50 @@ class Member{
 		this.pass = pass;
 	}
 
-	// 아이디 중복확인 메서드
-	private boolean equalsId(String id) {
-		return this.id == id;
+	public String getEamil() {
+		return eamil;
+	}
+
+	public void setEamil(String eamil) {
+		this.eamil = eamil;
 	}
 	
-	// 비밀번호 중복확인 메서드
-	private boolean equalsPass(String pass) {
-		return this.pass == pass;
+	
+	// equals : 아이디,비번이 주어지면 내 정보와 같은지 알려주는 메서드
+	public boolean eqauals(String id, String pass) {
+		// 아이디가 없거나
+		if(this.id == null) {
+			return false;
+		}
+		// 비번이 없거나
+		if(this.pass == null) {
+			return false;
+		}
+		// 아이디와 비번이 일치하면
+		if(this.id.equals(id) && this.pass.equals(pass)) {
+			return true;
+		}
+		return false;
+	}
+
+	// 회원정보를 출력하는 메서드
+	public void print() {
+		System.out.println("아이디 : " + id);
+		System.out.println("비밀번호 : " + pass);
+		System.out.println("이메일 : " + eamil);
 	}
 	
-	// 생성자
+	// 생성자 : 기본. 아이디,비번,이메일이 필요한 생성자. 아이디,비번이 필요한 생성자
+	public Member() {
+		
+	}
+	
+	public Member(String id, String pass, String eamil) {
+		this.id = id;
+		this.pass = pass;
+		this.eamil = eamil;
+	}
+
 	public Member(String id, String pass) {
 		this.id = id;
 		this.pass = pass;
