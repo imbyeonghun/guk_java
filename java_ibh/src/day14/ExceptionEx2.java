7c135c87d7e36fac99fc45e19e5e7ab8223355f8
imbyeonghun <byeonghun6885@naver.com>
2024-01-01 16:39:58 +0900

    Exception예제

---
+++ java_ibh/src/day14/ExceptionEx2.java
@@ -0,0 +1,24 @@
+package day14;
+
+public class ExceptionEx2 {
+
+	public static void main(String[] args) {
+		
+		test();
+	}
+
+	public static void test(){
+		int num1 = 1, num2 = 0;
+		try {
+			System.out.println(num1 / num2);
+		}catch(ArithmeticException e) {
+			System.out.println("0으로 나눌수없습니다.");
+			return;
+		}finally {	
+			// 위에서 예외가 발생해서 처리하고, return으로 빠져나가도 실행
+			System.out.println("finally 실행");
+		}
+		// 위에서 예외가 발생해서 return으로 빠져나가면 실행X
+		System.out.println("메서드 종료");
+	}
+}
