3b7ab40b3a2857311eb4d9c97f0e847e759b024d
imbyeonghun <byeonghun6885@naver.com>
2024-01-05 08:48:21 +0900

    서버 이용한 학생 관리 예제

---
+++ java_ibh/src/day18/student/StudentManager.java
@@ -0,0 +1,45 @@
+package day18.student;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class StudentManager {
+
+	private List<Student> list = new ArrayList<Student>();
+
+	public StudentManager(List<Student> list) {
+		if(list == null) {
+			return;
+		}
+		this.list = list; 
+	}
+
+	public boolean insertStudent(Student std) {
+		if(list.contains(std)) {
+			return false;
+		}
+		list.add(std);
+		return true;
+	}
+
+	public void printList() {
+		list.stream().forEach(s->System.out.println(s));
+	}
+
+	public boolean updateStudent(Student std) {
+		// 등록된 학생인지 확인
+		int index = list.indexOf(std);
+		
+		// 등록된 학생이 아니면 false 리턴
+		if(index < 0) {
+			return false;
+		}
+		
+		// 등록된 학생이면 이름을 수정
+		list.get(index).setName(std.getName());
+		return true;
+	}
+	
+}
