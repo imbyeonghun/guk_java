e345cca0b7568fd3e8266a9bb35c034abdd55c8b
imbyeonghun <byeonghun6885@naver.com>
2024-03-13 16:46:15 +0900

    스프링 게시글 리스트 구현중

--- spring/spring/src/main/java/kr/kh/spring/dao/MemberDAO.java
+++ spring/spring/src/main/java/kr/kh/spring/dao/MemberDAO.java
@@ -3,9 +3,7 @@ package kr.kh.spring.dao;
 import kr.kh.spring.model.vo.MemberVO;
 
 public interface MemberDAO {
-	//@Param("member") 
 	boolean insertMember(MemberVO member);
 
 	MemberVO selectMember(String me_id);
-
 }
